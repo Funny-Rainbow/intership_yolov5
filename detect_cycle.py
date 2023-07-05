24e5c5cc70d9626a11711887d481859ab47bab12
Funny-Rainbow <1419009509@qq.com>
2023-07-05 15:18:24 +0800

    my first commit

---
+++ detect_cycle.py
@@ -0,0 +1,108 @@
+import os
+import shutil
+import datetime
+import cv2
+import argparse
+from time import sleep, localtime, time
+
+import my_detect, sendToSQL
+"""
+参数
+set_time 选择一天中的一个时间来运行文件
+source  照片源（应该为所有设备所在的路径）
+temp    创建一个暂时存储识别照片的文件夹，识别结束后会删除
+"""
+source = r'H:\backup\files\jsy-camera\cameraCapture'
+temp = r'D:\Deep Learning\yolov5-master\my_temp\images'
+
+def copy_files(set_date, set_time, source, temp):
+    dir_list = os.listdir(source)# 遍历设备
+    # 遍历图片
+    for i in dir_list:
+        file_path = source + '\\' + str(i)
+        file_list = os.listdir(file_path)
+        for j in file_list:
+            # 只复制每天09:00和14:00拍摄的图片
+            time_ok = j[8:10] == '09' or j[8:10] == '14'
+            if j[0:8] == set_date and time_ok:
+                hello, fileType = j.rsplit('.')
+                name, suffix = j.rsplit('.' + fileType)
+                name = name.replace('.', '')
+                old_name = file_path + '\\' + j
+                new_name = temp + '\\' + i + '_' + name + "." + fileType #文件夹名+文件名+格式
+                shutil.copyfile(old_name, new_name)
+
+
+#显示识别到的疑似非农化照片
+def show_images(source, detected_files):
+     print("识别到",len(detected_files),"张疑似非农化照片")
+     for i in range(len(detected_files)):
+          id, filename = detected_files[i][0].split("_")
+          url = source + "\\" + str(id) + "\\" + str(filename)
+          img = cv2.imread(url)
+          img = cv2.resize(img, (192*m,108*m))
+          cv2.imshow("img", img)
+          cv2.waitKey(0)
+          cv2.destroyAllWindows()
+
+def main(set_date,set_time, source, temp):
+    # 只有设定的那一个小时才执行一次
+    if int(localtime(time())[3]) == set_time:
+        try:
+            os.mkdir(temp)
+        except:
+            print(Exception.args)
+        copy_files(set_date, set_time, source, temp) # 将今天的照片临时拷贝到同一目录下
+        tempDir = os.listdir(temp)
+        # 判断暂存文件夹内有无图片
+        print('找到了',len(tempDir),'张照片')
+        if len(tempDir):
+            opt = my_detect.parse_opt() # 获取需要传入ai识别的参数
+            detected_files = my_detect.main(opt) # 返回识别为疑似非农化的图片名称以及置信度
+            show_images(source, detected_files) # 从备份文件中查看疑似非农化的图片
+        else:
+            detected_files = None
+            print('未发现今日图片，停止识别')
+        try:
+                shutil.rmtree(temp) # 每次运行结束后，删除临时照片，每次运行会因为未知原因报错，不影响使用
+        except:
+                print(Exception.with_traceback, '这个报错有时出现，但是能正常执行')
+        
+        return detected_files
+    else:
+         print('不在指定工作时间')
+
+
+def parse_opt():
+    
+    today = str(datetime.date.today())
+    today_splited = today.split('-')# 分离年月日
+    today = ''
+    today = str(today_splited[0])+str(today_splited[1]+str(today_splited[2])) #获取今天的日期
+    #today = '20230622' #仅限测试使用
+    now = localtime(time())[3]
+    parser = argparse.ArgumentParser()
+    args, unknown = parser.parse_known_args()
+    parser.add_argument('--set_date',  type=str, default= today, help='only for test, just leave it defult')
+    parser.add_argument('--set_time',  type=int, default= now, help='choose when does the process run')
+    parser.add_argument('--source', type=str, default= source, help='path of the device root(not photos root)')
+    #parser.add_argument('--source', type=str, default= r'H:\backup\files\jsy-camera\cameraCapture', help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--temp', type=str, default= temp, help='temprorarily create a folder to store photos')
+    myopt, unknown = parser.parse_known_args()
+    if unknown:
+         print('Unknown in my_main.py arguments:', unknown)
+    return myopt
+
+
+if __name__ == '__main__':
+    m = 6   #设置照片显示大小
+    while True:
+        myopt = parse_opt()
+        print('parameters:',myopt)
+        detected_files = main(**vars(myopt))
+        # id,create_time, name, confidence
+        if detected_files:
+            sendToSQL.s2S(detected_files)
+            print('识别到了：', len(detected_files),'张非农化照片')
+        print("休眠,现在时间", localtime(time())[3],"时")
+        sleep(60*60)
---
+++ sendToSQL.py
@@ -0,0 +1,81 @@
+import pymysql
+import datetime
+import argparse
+# 数据库格式：
+#     1          2                  3             4                  5
+# id(int), create_time(datetime), device(str), file_name(str), confidence(float)
+
+# 连接数据库
+def init(db_host,db_user,db_pwd,db_database):
+    db = pymysql.connect(host=db_host, user= db_user, password=db_pwd, database=db_database)
+    # 使用cursor()创建一个cursor对象
+    cursor = db.cursor()
+    return db, cursor
+
+# 数据列表
+def dataInit(cursor, old_data):
+    cursor.execute('SELECT max(id) FROM cvdata')
+    max_id = cursor.fetchone()[0]
+    if max_id:
+        id = max_id
+    else:
+        id = 0
+    print(id,type(id))
+    data = []
+    for i in old_data:
+        id = id + 1
+        confidence = i[1]
+        now = datetime.datetime.now()
+        device, name = i[0].split('_')
+        new_data = [id, now, device, name, confidence]
+        data.append(new_data)
+    
+    return data
+
+def send(db, cursor, data):
+    try:
+    # 执行SQL,插入多条数据
+        print(data)
+        cursor.executemany("insert into cvdata(id,create_time, device, file_name, confidence) values (%s,%s,%s,%s,%s)", data)
+
+        # 提交数据
+        db.commit()
+        print('6')
+    
+    except Exception as error:
+        # 发生错误时回滚
+        db.rollback()
+        print('database error occurs:',error)
+
+    # 关闭数据库连接
+    db.close()
+
+# 主函数  
+def s2S(old_data):
+    #获取参数
+    dbopt = parse_opt()
+    #数据库初始化
+    db,cursor = init(**vars(dbopt))
+    #数据处理
+    data = dataInit(cursor, old_data)
+    #发送到数据库
+    send(db, cursor, data)
+
+# 创建参数
+def parse_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--db_host',  type=str, default= '127.0.0.1', help='database host')
+    parser.add_argument('--db_user', type=str, default= 'root', help='database user')
+    #parser.add_argument('--source', type=str, default= r'H:\backup\files\jsy-camera\cameraCapture', help='file/dir/URL/glob/screen/0(webcam)')
+    parser.add_argument('--db_pwd', type=str, default= '##JmMyC2810', help='database password')
+    parser.add_argument('--db_database', type=str, default= 'cvtest', help='database name')
+    dbopt, unknown = parser.parse_known_args()
+    if unknown:
+         print('Unknown in sendToSQL.py arguments:', unknown)
+    return dbopt
+
+if __name__ == '__main__':
+    old_data = [
+    ("2c948099823b15ea018275d2e0926f7f_20230622092256.jpg", "0.80"),
+    ( "2c94809984a437180184d21c579043e8_20230622092744.jpg", "0.90"),]
+    s2S(old_data)
